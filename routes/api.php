53afdbc1699331354dbc976fdc79b238b5ae6156
Julio Mafra <julioclmafra@gmail.com>
2023-07-10 18:20:16 -0300

    iniciando request em testamento

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestamentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/teste', function(){
     return 'teste com sucesso!';
 });
+Route::post('/testamento',[TestamentoController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
