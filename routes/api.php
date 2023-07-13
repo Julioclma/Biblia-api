0f0adbbde3027708707f5e74340c56eec815a4ee
Julio Mafra <julioclmafra@gmail.com>
2023-07-13 15:29:51 -0300

    criando rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LivroController;
 use App\Http\Controllers\TestamentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,14 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/teste', function(){
-    return 'teste com sucesso!';
-});
-Route::post('/testamento',[TestamentoController::class, 'store']);
 Route::get('/testamento', [TestamentoController::class, 'index']);
+Route::post('/testamento', [TestamentoController::class, 'store']);
 Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
 Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
 Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
+
+Route::get('/livro', [LivroController::class, 'index']);
+Route::get('/livro/{id}', [LivroController::class, 'show']);
+Route::put('/livro/{id}', [LivroController::class, 'update']);
+Route::post('/livro', [LivroController::class, 'store']);
+Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
